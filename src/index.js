6ad667bf9e191ea0b3530908ed74834062f155fd
evatrilinfo <evatrilinfo@gmail.com>
2025-11-10 12:04:34 +0530

    updated context

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // src/index.js
 import Navbar from "../src/components/Navbar";
+import { CityFilterProvider, useCityFilter } from "../src/Context/CityFilterContext";
 
-export { Navbar };
+export { Navbar,CityFilterProvider,useCityFilter };
 export default Navbar;
